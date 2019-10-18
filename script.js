6c7f4a6ab7e2fc4e9afc60b3a12b74cb75a9c6ef
Ubuntu <ubuntu@ip-172-31-95-225.ec2.internal>
2019-10-18 07:54:44 +0000

    added styles to map app

--- script.js
+++ script.js
@@ -47,3 +47,16 @@ function setupMap()
     accessToken: mapboxgl.accessToken,
     mapboxgl: mapboxgl
     }),'top-left');
+    
+    //add style to map
+    var layerList = document.getElementById('menu');
+    var inputs = layerList.getElementsByTagName('input');
+ 
+    function switchLayer(layer) {
+    var layerId = layer.target.id;
+    map.setStyle('mapbox://styles/mapbox/' + layerId);
+    }
+ 
+    for (var i = 0; i < inputs.length; i++) {
+    inputs[i].onclick = switchLayer;
+    }
